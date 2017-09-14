e3578f9f283f18676e264f367bc6fa1c93ca089c
Ganlv <ganlvtech@qq.com>
2017-09-14 18:08:46 +0800

    修复筹款页二维码事件对象错误

--- src/js/money.js
+++ src/js/money.js
@@ -2,8 +2,7 @@ function showQR() {
     $("#QRcode").css("display", "block");
     $('#QRcode').closest('.swiper-slide').addClass('swiper-no-swiping');
 }
-$("#QRcode").click(function(e) {
-    if ($(e.target).attr("id") == "hideQR")
-        $("#QRcode").css("display", "none");
+$("#hideQR").click(function(e) {
+    $("#QRcode").css("display", "none");
     $('#QRcode').closest('.swiper-slide').removeClass('swiper-no-swiping');
 });
